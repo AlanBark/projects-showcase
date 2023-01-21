e3af43a6f1f2ee3ddd6ddde0a0caca54b36b116e
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2023-01-21 20:24:23 +1000

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from '$app/stores';
+</script>
+
+<h1>Oops!</h1>
+<p>Looks like something went wrong.</p>
+<h1>{$page.status}: {$page.error?.message}</h1>
--- src/routes/projects/[slug]/+page.ts
+++ src/routes/projects/[slug]/+page.ts
@@ -2,14 +2,19 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
  
 export const load = (async ({ params }) => {
-  const post = await import(`../${params.slug}.md`);
-  const { title, date } = post.metadata;
-  const content = post.default;
-  
-  return {
-    content,
-    title,
-    date
+  try {
+
+    const post = await import(`../${params.slug}.md`);
+    const { title, date } = post.metadata;
+    const content = post.default;
+    
+    return {
+      content,
+      title,
+      date
+    }
+  } catch {
+    throw error(404, 'Not found')
   }
   
 }) satisfies PageLoad;
