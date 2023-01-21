21efd89dd00c2cd9f1e2d7761914ed3186a9db55
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2023-01-21 20:13:07 +1000

    initial commit

---
+++ src/routes/projects/[slug]/+page.ts
@@ -0,0 +1,15 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+ 
+export const load = (async ({ params }) => {
+  const post = await import(`../${params.slug}.md`);
+  const { title, date } = post.metadata;
+  const content = post.default;
+  
+  return {
+    content,
+    title,
+    date
+  }
+  
+}) satisfies PageLoad;
---
+++ src/routes/projects/test.md
@@ -0,0 +1,12 @@
+---
+title: Post One
+date: "2021-12-14"
+---
+
+Hello, I am _Post One._
+
+**Nice to meet you!**
+
+```js
+const asdf = 123;
+```
