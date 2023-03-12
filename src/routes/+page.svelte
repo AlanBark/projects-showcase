648e29127301a25e1ecfc6107dfb6db18865dd82
Alec Parkes <57089930+AlanBark@users.noreply.github.com>
2023-03-13 02:05:53 +1000

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,17 +43,17 @@ $far: #04724D;
   transform: translateZ(-2px) scale(3);
 }
 
-$layers: 6;
+$layers: 2;
 
 @for $i from 0 through $layers {
   $x: math.div($layers - $i, 2);
   $color: mix($close, $far, math.div($i, $layers) * 100 * 1%);
   .layer_#{$i}{
     transform: translateZ(-2 * $x - 2px) scale($layers - $i + 3);
-    background-image: linear-gradient(135deg, transparent 66%, $color 66.01%), linear-gradient(45deg, $color 34%, transparent 34.01%);
-      background-position: $x*200px $x*120*-1px;
-      background-size: 200px 100%;
-      background-repeat:repeat-x;
+    // background-image: linear-gradient(135deg, transparent 66%, $color 66.01%), linear-gradient(45deg, $color 34%, transparent 34.01%);
+    //   background-position: $x*200px $x*120*-1px;
+    //   background-size: 200px 100%;
+    //   background-repeat:repeat-x;
   }
 }
 
@@ -61,26 +61,28 @@ $layers: 6;
 
 <div class="parallax">
   <div class="layer layer_0">
+    <img src="/atop.svg" alt="abot" style="width: 100%; height: 100%; object-fit: cover;">
   </div>
   <div class="layer layer_1">
+    <img src="/abot.svg" alt="abot" style="width: 100%; height: 100%; object-fit: cover;">
   </div>
-  <div class="layer layer_2">
+  <!-- <div class="layer layer_2">
   </div>
   <div class="layer layer_3">
   </div>
   <div class="layer layer_4">
-  </div>
+  </div> 
   <div class="layer layer_5">
   </div>
   <div class="layer layer_6">
-  </div>
+  </div> -->
   <div class="parallax_cover">
     <section>
       <h1>Parallax</h1>
       <p>Scroll down to see the effect</p>
     </section>
   </div>
-</div>
+</div> 
 
 
 
